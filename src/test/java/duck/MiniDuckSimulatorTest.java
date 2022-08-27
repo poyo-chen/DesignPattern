daec016c5b2db7d2c848465b401df53a0f3e0507
martinezchen <martinezchen01@gmail.com>
2022-08-27 17:23:21 +0800

    strategy pattern sample

---
+++ src/test/java/duck/MiniDuckSimulatorTest.java
@@ -0,0 +1,27 @@
+package duck;
+
+import org.junit.jupiter.api.Test;
+import start.behavior.FlyRocketPowered;
+import start.duck.Duck;
+import start.duck.MallardDuck;
+import start.duck.ModelDuck;
+
+public class MiniDuckSimulatorTest {
+
+  @Test
+  void TestMallardDuck() {
+    Duck mallard = new MallardDuck();
+    mallard.performQuack();
+    mallard.performFly();
+  }
+
+  @Test
+  void TestModelDuck() {
+    Duck model = new ModelDuck();
+    model.performFly();
+    model.setFlyBehavior(new FlyRocketPowered());
+    model.performFly();
+  }
+
+
+}
